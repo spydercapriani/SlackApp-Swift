dcd6ba3fde38bc07494071dba6ad224b4724821f
Danny Gilbert <dgilbert@foreflight.com>
2023-02-14 10:48:46 -0600

    Adds check for Docker in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,5 +6,8 @@
 # Install brew dependencies
 brew bundle
 
+#Open Docker, only if is not running
+pgrep -f Docker.app > /dev/null || (echo "Starting Docker.app" && open /Applications/Docker.app)
+
 # Open Project in Xcode
 xed Package.swift
